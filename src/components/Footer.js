1f38ce9b4b51e7b31f7717152d6470fa53ceb9d0
hassan1400938 <mohammadwaqarulhassan@gmail.com>
2023-09-21 11:00:27 +0500

    add social media links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,16 +9,16 @@ export default function Footer() {
   return (
     <footer>
       <div className="socialIcons">
-        <a href="#">
+        <a href="https://web.facebook.com/mohammadwaqarulhassan1800">
           <img src={fbIcon}></img>
         </a>
-        <a href="#">
+        <a href="https://github.com/hassan1400938/">
           <img src={ghIcon}></img>
         </a>
-        <a href="#">
+        <a href="https://www.instagram.com/rixo_sol/">
           <img src={igIcon}></img>
         </a>
-        <a href="#">
+        <a href="https://twitter.com/MuhammadW119">
           <img src={twIcon}></img>
         </a>
       </div>
